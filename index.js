0de3e5d413c2fd8300fbe928c575244bd5d4cbe8
Alp Eren Sağdıç <125905845+AlpSagdic@users.noreply.github.com>
2023-08-01 14:02:39 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs")
+});
+
+app.post("/", async (req, res) => {
+    try {
+        const result = await axios.get(`https://v2.jokeapi.dev/joke/Programming?type=single`);
+        res.render("index.ejs", { joke: result.data.joke})
+    } catch (error) {
+        console.error(error.response.data);
+        res.status(500);
+    }
+});
+
+console.log("deneme")
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+});
